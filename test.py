f5492f859b1221b5d1f54c3c23a52d552ad79e6c
MarvinReese <89393824+MarvinReese@users.noreply.github.com>
2021-09-09 13:26:58 +0200

    Add files via upload
    Variablen verändert und eine Liste für die Begleiter hinzugefügt

--- test.py
+++ test.py
@@ -1,26 +1,41 @@
-#Testingground für Variablen und if abfragen
+#Testingground für Variablen und If abfragen
 
-name = input(('Whats you name?'))
-print('Ok, your name is ' + name)
+player_name = input ('Whats you name? ')
+print('Ok, your name is ' + player_name)
 
-if str(name) == 'Marvin':
+if str(player_name) == 'Marvin':
     print('Oh, you are my creator!')
 
-age = input(('How old are you?'))
+age = input('How old are you? ')
 print('You are ' + str(age) + ' years old.')
 
 if int(age) < 18:
     print\
         ('You are not allowed to proceed.')
+    exit()
+
 elif int(age) == 18:
     print\
         ('Ah you are a lucky one, you are allowed to proceed, loading...')
+
 else:
     print\
-    ('You are allowed to proceed, loading...')
+        ('You are allowed to proceed, loading...')
 
 if int(age) >= 18:
-        print('What can i do for you ' + name +'?')
+        print('Ok ' + player_name + ' its time to choose your companions!')
 
-#githubtest
+#Testingground für Listen
+
+first_companion_name = input("What is the name of your first companion? ")
+print("Ok " + first_companion_name + " is on your side")
+
+second_companion_name = input("What is the name of your second companion? ")
+print("Ok " + second_companion_name + " is on your side")
+
+companions = [first_companion_name, second_companion_name]
+print (companions)
+
+print("Press Enter to continue")
+input()
 
